35ad353cbcc32d52dff807a1b5cc901fb23e7e84
divyakhichi14 <divyanka1401@gmail.com>
2022-02-24 10:21:02 +0530

    Deleted fn header

--- math.py
+++ math.py
@@ -1,16 +1,15 @@
-#Add Implementation
+
 def add(x,y):
     return x+y
 
-#Subtract Implementation
 def subtract(x,y):
     return x-y       #on master
 
-#Multiply Implementation
+
 def multiply(x,y):
     return x*y           #on bug456
 
-#Divide Implementation
+
 def divide(x,y):
     if y==0:
         return divide_by_0_error
